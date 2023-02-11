e6d2af84e31f3c36f66f4aaa24c6274e8deae114
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2023-02-11 11:10:25 -0500

    added javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+
+async function main() {
+    let sd = document.querySelector('.software-development-section');
+    console.log(sd)
+
+}
+
+document.addEventListener('DOMContentLoaded', async () => main())
