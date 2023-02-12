a4641507da19c1deac3201dfe227d912ba98b62a
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2023-02-12 16:00:19 -0500

    added box-highlighting to skill divs

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 
-async function main() {
-    let sd = document.querySelector('.software-development-section');
-    console.log(sd)
+function main() {
+    // Get all the children in the skills section
+    const skills_section = document.querySelector('.skills-section-wrapper').childNodes; 
 
+    // 
+    skills_section.forEach((item) => {
+        
+        // Execute when the user's pointer is over the software-development-section
+        item.addEventListener('pointerover', () => {
+            // Change the color of the div's border to white
+            item.style.borderColor = 'black';
+        });
+        
+        // Execute when the user's pointer leaves the software-development-section
+        item.addEventListener('pointerout', () => {
+            // Change the color of the div's border to white
+            item.style.borderColor = 'white';
+        });
+
+        item.addEventListener('click', () => {
+            console.log(`Go to ${item.dataset.url}`); 
+            window.open(`${item.dataset.url}`);
+        }); 
+    });
 }
-
-document.addEventListener('DOMContentLoaded', async () => main())
+document.addEventListener('DOMContentLoaded', () => main())
